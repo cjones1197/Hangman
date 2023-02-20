7a8fc2de169744f8cbc7ebbb79af7e38fe974f2b
cjones1197 <122645490+cjones1197@users.noreply.github.com>
2023-02-20 23:10:41 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Imported Body component from the Components folder.
+import Body from "./Components/Body";
+// Imported Header component from the Components folder.
+import Header from "./Components/Header";
+
+function App() {
+
+  return (
+    <div className="main-container">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+// The App function is exported.
+export default App;
