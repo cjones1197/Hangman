4da51e45deed5d54e055c6b5719860c26ba1a409
cjones1197 <122645490+cjones1197@users.noreply.github.com>
2023-02-20 23:18:32 +0000

    Initial commit

---
+++ src/Store/store.js
@@ -0,0 +1,12 @@
+// Imported the configureStore function from the redux toolkit.
+import { configureStore } from "@reduxjs/toolkit";
+// Imported the reducers from account.js
+import wordReducer from "../Features/wordSlice";
+
+//Exported configureStore and set it to respond to the reducers that have been created in wordSlice.js
+export default configureStore({
+    reducer: {
+        word: wordReducer,
+    },
+}
+);
